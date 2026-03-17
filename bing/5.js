e785a20128afaa6f276d1855796165923e4a1acb
MHuiG <MHuiG@users.noreply.github.com>
2026-03-17 21:40:27 +0000

    deploy: 97aaf9e60d2c05c97d45cac4958a0a743ad0cf5b

--- bing/5.js
+++ bing/5.js
@@ -1,1 +1,1 @@
-document.write("Peach trees in bloom, Cieza, Murcia, Spain (© Juan Maria Coy Vergara/Getty Images)")
+document.write("Juvenile sunbittern displaying at nest, Ecuador (© Andy Rouse/naturepl.com)")
